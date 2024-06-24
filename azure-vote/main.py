73fc9b5ef6e9bb48d5010744cac9cffc72f364a5
Hoai Nguyen <hoainguyen.1128@gmail.com>
2024-06-24 15:29:55 +0700

    Initial commit for Deploy_to_VMSS branch

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -6,23 +6,48 @@ import socket
 import sys
 import logging
 from datetime import datetime
-
+from opencensus.ext.azure.log_exporter import AzureLogHandler, AzureEventHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 # App Insights
 # TODO: Import required libraries for App Insights
 
 # Logging
-logger = # TODO: Setup logger
+logger = logging.getLogger(__name__)# TODO: Setup logger
+logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=00769094-6f68-4a1c-b4c6-09dc7c1c8f22;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=5a698c08-60e0-4170-b650-5eeb9b8e02e0'))
+logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=00769094-6f68-4a1c-b4c6-09dc7c1c8f22;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=5a698c08-60e0-4170-b650-5eeb9b8e02e0'))
 
 # Metrics
-exporter = # TODO: Setup exporter
+exporter = metrics_exporter.new_metrics_exporter(
+  enable_standard_metrics=True,
+  connection_string='InstrumentationKey=00769094-6f68-4a1c-b4c6-09dc7c1c8f22;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=5a698c08-60e0-4170-b650-5eeb9b8e02e0')
+# TODO: Setup exporter
 
 # Tracing
-tracer = # TODO: Setup tracer
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=00769094-6f68-4a1c-b4c6-09dc7c1c8f22;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=5a698c08-60e0-4170-b650-5eeb9b8e02e0'),
+    sampler=ProbabilitySampler(1.0),
+)
+# TODO: Setup tracer
 
 app = Flask(__name__)
 
 # Requests
-middleware = # TODO: Setup flask middleware
+middleware =  FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string="InstrumentationKey=00769094-6f68-4a1c-b4c6-09dc7c1c8f22;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=5a698c08-60e0-4170-b650-5eeb9b8e02e0"),
+    sampler=ProbabilitySampler(rate=1.0),
+)
+# TODO: Setup flask middleware
 
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
@@ -43,7 +68,20 @@ else:
     title = app.config['TITLE']
 
 # Redis Connection
-r = redis.Redis()
+if ("REDIS" in os.environ and os.environ['REDIS']):
+    redis_server = os.environ['REDIS']
+else:
+    redis_server = app.config['REDIS']
+try:
+   if "REDIS_PWD" in os.environ:
+      r = redis.StrictRedis(host=redis_server,
+                        port=6379,
+                        password=os.environ['REDIS_PWD'])
+   else:
+      r = redis.Redis(redis_server)
+   r.ping()
+except redis.ConnectionError:
+   exit('Failed to connect to Redis, terminating.')
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
@@ -60,8 +98,10 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
+        tracer.span(name="Total {} Voted: {}".format(button1, vote1))
         # TODO: use tracer object to trace cat vote
         vote2 = r.get(button2).decode('utf-8')
+        tracer.span(name="Total {} Voted: {}".format(button2, vote2))
         # TODO: use tracer object to trace dog vote
 
         # Return index with values
@@ -76,10 +116,12 @@ def index():
             r.set(button2,0)
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            logger.warning("{} voted".format(button1), extra=properties)
             # TODO: use logger object to log cat vote
-
+            
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            logger.warning("{} voted".format(button2), extra=properties)
             # TODO: use logger object to log dog vote
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -99,6 +141,6 @@ def index():
 
 if __name__ == "__main__":
     # TODO: Use the statement below when running locally
-    app.run() 
+    # app.run() 
     # TODO: Use the statement below before deployment to VMSS
-    # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
